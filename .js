08b1221dcb23ac8430c35b364f62c08be1d8f55d
Joshua M Taylor <joshua.taylor.nola@gmail.com>
2017-07-20 08:44:23 -0500

    Update .js

--- .js
+++ .js
@@ -1,3 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+class Application extends React.Component {
+  render() {
+    return <div>
+      <h1>Orcanus Dark</h1>
+      <p>
+        "In days to come, the darkness will begin to consume the light. 
+During these days many small candles will burn together, brighter than
+the sun and push the shadows back." - Elder Master Lucian Ethalas
+      </p>
+      <div>
+       <p>Welcome warrior. By what name shall you be known by?</p>
+       </div>
+    </div>;
+  }
+}
+
+//user name input
+
+
+
+React.render(<Application />, document.getElementById('app'));
