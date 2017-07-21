dc573bbba0e43260d0542a8a69fb2537a4fa21ba
Joshua M Taylor <joshua.taylor.nola@gmail.com>
2017-07-21 09:35:13 -0500

    Update events.js

--- events.js
+++ events.js
@@ -1,2 +1,10 @@
 
 var warInput=document.getById("war-name");
+var enterName=document.getById("enter-name);
+                               
+enterName.addEventListener("click", function (){
+  var warname = (warInput.value);
+  var welcomewarrior = "Greetings"
+  greeting.innerHTML= welcomewarrior + warname + ".";
+});
+
